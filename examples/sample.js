54993a11ee73640929d81f24586080173a4559b7
momamene <momamene@gmail.com>
2017-04-21 06:30:27 +0000

    Initial commit

---
+++ examples/sample.js
@@ -0,0 +1,34 @@
+const data = {
+  "input": [{
+    "storage": "azure",
+    "path": {
+      "containerName": "videos",
+      "blobName": "nature.mp4"
+    },
+    "ffmpegOptions": [
+    ]
+  }],
+  "output": [{
+    "storage": "azure",
+    "path": {
+      "containerName": "videos",
+      "blobName": "nature2.mp4"
+    },
+    "ffmpegOptions": [
+      "-f avi"
+    ]
+  }, {
+    "storage": "azure",
+    "path": {
+      "containerName": "videos",
+      "blobName": "nature3.mp4"
+    },
+    "ffmpegOptions": [
+      "-f avi"
+    ]
+  }]
+};
+
+const CloudFFmpeg = require('./lib/cloud-ffmpeg');
+
+let cloudFFmpeg = new CloudFFmpeg(data);
